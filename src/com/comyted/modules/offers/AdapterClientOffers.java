c1ebd96ade5900996d5ee19668a73a77ab32e482
ansel86castro <ansel86castro@gmail.com>
2015-10-05 13:16:27 -0500

    05/10/2015

--- src/com/comyted/modules/offers/AdapterClientOffers.java
+++ src/com/comyted/modules/offers/AdapterClientOffers.java
@@ -2,6 +2,8 @@ package com.comyted.modules.offers;
 
 import android.content.Context;
 import android.view.View;
+import android.widget.TextView;
+
 import com.comyted.R;
 import com.comyted.generics.ListAdapter;
 import com.comyted.models.ClientOffert;
@@ -21,5 +23,21 @@ public class AdapterClientOffers extends ListAdapter<ClientOffert> {
 		UIUtils.setTextViewText(view,R.id.oferta_responsable, item.responsable);
 		UIUtils.setTextViewText(view,R.id.oferta_tipo, item.tipooferta);
 		UIUtils.setTextViewText(view, R.id.oferta_estado, item.estado);	
+		
+		TextView estado = (TextView) view.findViewById(R.id.oferta_estado);		
+		if(item.estado.equalsIgnoreCase("Aceptada")){
+			estado.setBackgroundResource(R.color.aceptada);
+		}else if(item.estado.equalsIgnoreCase("Aplazada")){
+			estado.setBackgroundResource(R.color.aplazada);
+		}else if(item.estado.equalsIgnoreCase("Bloqueada")){
+			estado.setBackgroundResource(R.color.bloqueada);
+		}else if(item.estado.equalsIgnoreCase("Denegada")){
+			estado.setBackgroundResource(R.color.denegada);
+		}else if(item.estado.equalsIgnoreCase("En Espera")){
+			estado.setBackgroundResource(R.color.en_espera);
+		}else if(item.estado.equalsIgnoreCase("Presentada")){
+			estado.setBackgroundResource(R.color.presentada);
+		}else
+			view.setBackgroundResource(0);
 	}
 }
